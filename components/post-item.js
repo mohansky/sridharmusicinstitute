39169270875ae4608ca864f82be8de53f683213e
mohansky <38560439+mohansky@users.noreply.github.com>
2022-04-26 12:35:17 +0530

    Add files via upload

--- components/post-item.js
+++ components/post-item.js
@@ -67,7 +67,7 @@ export default function PostItem({
                   </label>
                 </p>
                 <input type="hidden" name="form-name" value="demo" />
-                {/* <input type="hidden" value={title} /> */}
+                <input id="hidden_field" name="hidden_field"  type="hidden" value={title} />
                 <Form.Group className="mb-3" controlId="formBasicName">
                   <Form.Control type="name" name="name" placeholder="Name" />
                 </Form.Group>
