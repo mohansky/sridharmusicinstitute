f9c89cdf08a9cc0938644bc78cb5b5ec91bee6ae
mohansky <38560439+mohansky@users.noreply.github.com>
2022-04-26 18:43:30 +0530

    Add files via upload

--- components/layout.js
+++ components/layout.js
@@ -1,35 +1,13 @@
-import * as React from "react" 
-import Header from "./header"
-import Footer from "./footer"
+import * as React from "react";
+import Header from "./header";
+import Footer from "./footer";
 
-
-export default function Layout ({ location, title, children }) {
-  // const rootPath = `${__PATH_PREFIX__}/`
-  // const isRootPath = location.pathname === rootPath
-  // let header
-
-  // if (isRootPath) {
-  //   header = (
-  //     <h1 className="main-heading">
-  //       <Link to="/">{title}</Link>
-  //     </h1>
-  //   )
-  // } else {
-  //   header = (
-  //     <Link className="header-link-home" to="/">
-  //       {title}
-  //     </Link>
-  //   )
-  // }
-
+export default function Layout({ children }) {
   return (
-    <div className="global-wrapper">
-    {/* <div className="global-wrapper" data-is-root-path={isRootPath}> */}
-      <Header/>
-      {/* <header className="global-header">{header}</header> */}
+    <>
+      <Header />
       <main>{children}</main>
-      <Footer/>
-    </div>
-  )
+      <Footer />
+    </>
+  );
 }
-
