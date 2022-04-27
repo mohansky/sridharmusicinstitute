d259076ae86285468dccbab6839f711ec231a380
mohansky <38560439+mohansky@users.noreply.github.com>
2022-04-27 12:38:38 +0530

    Add files via upload

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,33 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang='en'>
-        <Head />
-        <body> 
+      <Html lang="en">
+        <Head>
+          <NextScript
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MWPMZ8K');`,
+            }}
+          />
+        </Head>
+
+        <body>
+          {/* <!-- Google Tag Manager (noscript) --> */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-MWPMZ8K"
+              height="0"
+              width="0"
+              className="d-none"
+            ></iframe>
+          </noscript>
+          {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
         </body>
