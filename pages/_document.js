b22eb0263231ec892354219c16354066dacb7765
mohansky <38560439+mohansky@users.noreply.github.com>
2022-04-27 16:11:18 +0530

    Add files via upload

--- pages/_document.js
+++ pages/_document.js
@@ -9,10 +9,10 @@ class MyDocument extends Document {
             strategy="lazyOnload"
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-MWPMZ8K');`,
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NH46GHZ');`,
             }}
           />
         </Head>
@@ -21,12 +21,12 @@ class MyDocument extends Document {
           {/* <!-- Google Tag Manager (noscript) --> */}
           <noscript>
             <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-MWPMZ8K"
+              src="https://www.googletagmanager.com/ns.html?id=GTM-NH46GHZ"
               height="0"
               width="0"
               className="d-none"
             ></iframe>
-          </noscript>
+          </noscript> 
           {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
