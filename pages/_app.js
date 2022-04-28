3a51de6f178360a188f86eb23138d2760184324c
mohansky <38560439+mohansky@users.noreply.github.com>
2022-04-28 17:15:42 +0530

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,40 @@
-import React, { useEffect } from 'react';
-import '../styles/style.scss' 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Layout from '../components/layout' 
-import TagManager from "react-gtm-module"
-
-const tagManagerArgs = {
-  id: "GTM-NH46GHZ",
-}
+import React, { useEffect } from "react";
+import "../styles/style.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from "../components/layout";
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import { GTM_ID, pageview } from '../lib/gtm'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   useEffect(() => {
-    TagManager.initialize(tagManagerArgs)
-  }, [])
+    router.events.on("routeChangeComplete", pageview);
+    return () => {
+      router.events.off("routeChangeComplete", pageview);
+    };
+  }, [router.events]);
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )  
+    <>
+      {/* Google Tag Manager - Global base code */}
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
