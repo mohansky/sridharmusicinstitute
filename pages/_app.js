6ae7bd96082371a801ad008e6796fdf34f39a6af
mohansky <38560439+mohansky@users.noreply.github.com>
2022-04-29 12:56:54 +0530

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,15 @@ import React, { useEffect } from "react";
 import "../styles/style.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "../components/layout";
+import TagManager from 'react-gtm-module';
 import Script from 'next/script'
 import { useRouter } from 'next/router'
 import { GTM_ID, pageview } from '../lib/gtm'
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  useEffect(() => {
-    router.events.on("routeChangeComplete", pageview);
-    return () => {
-      router.events.off("routeChangeComplete", pageview);
-    };
-  }, [router.events]);
+    useEffect(() => {
+      TagManager.initialize({ gtmId: '${GTM_ID}' });
+  }, []);
 
   return (
     <> 
